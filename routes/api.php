3858da5b763993c493e4a24275a12b2e15ba863d
pond-e <haruto2266@icloud.com>
2025-07-14 23:17:10 +0900

    add-timer-log を認証必須に

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,11 @@ use App\Http\Controllers\Api\TimerLogController;
 
 Route::post('/register', [UserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/add-timer-log', [TimerLogController::class, 'store']);
 Route::get('/timer-logs', [TimerLogController::class, 'retrieve']);
 
 // 認証必須のルートをグループ化
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'me']);
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+    Route::post('/add-timer-log', [TimerLogController::class, 'store']);
 });
